b0efe554955631827c425cfffa12945c0ee65c5e
Luiz Gustavo <lg.lopesmelo@gmail.com>
2024-05-25 10:17:59 -0300

    Adicionar Spring

---
+++ src/main/java/com/whitelopes/liter/LiterApplication.java
@@ -0,0 +1,13 @@
+package com.whitelopes.liter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/whitelopes/liter/LiterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whitelopes.liter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
